ddb3f42ba137bc6897a1dcff613576faf555d906
Dylan Díaz <dydiazva@est.utn.ac.cr>
2025-11-04 16:40:24 -0600

    Proyect created
    Proyect created:
    register view added
    register view strings added
    person class added

---
+++ GeoJournal/app/src/main/java/com/ACID/geojournal/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.ACID.geojournal
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.OpenActivitybtn)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnopenactivity = findViewById<Button>(R.id.OpenActivitybtn)
+        btnopenactivity.setOnClickListener(View.OnClickListener{ view ->
+            Util.Util.openActivity(this, RegisterActivity::class.java)
+        })
+    }
+}
+
