a3b6a37ce1a652542c75f821c4be3efa40e39c9e
Dylan D <dydiazva@est.utn.ac.cr>
2025-11-05 16:39:54 -0600

    Update number 2
    Adding functionality to register activity

--- GeoJournal/app/src/main/java/com/ACID/geojournal/MainActivity.kt
+++ GeoJournal/app/src/main/java/com/ACID/geojournal/MainActivity.kt
@@ -7,13 +7,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.Button
+import com.ACID.geojournal.R.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.OpenActivitybtn)) { v, insets ->
+        setContentView(layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- GeoJournal/app/src/main/java/com/ACID/geojournal/RegisterActivity.kt
+++ GeoJournal/app/src/main/java/com/ACID/geojournal/RegisterActivity.kt
@@ -1,12 +1,37 @@
 package com.ACID.geojournal
 
+import Controller.PersonController
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Calendar
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
+    private lateinit var txtId: EditText
+    private lateinit var txtName: EditText
+    private lateinit var txtFLastName: EditText
+    private lateinit var txtSLastName: EditText
+    private lateinit var txtPhone: EditText
+    private lateinit var txtEmail: EditText
+    private lateinit var lbBirthdate: TextView
+
+    private  var day :Int=0
+    private  var month: Int=0
+    private  var year: Int =0
+
+    private var isEditMode: Boolean = false
+    private lateinit var personController: PersonController
+
+    private lateinit var menuItemDelete: MenuItem
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +41,33 @@ class RegisterActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        personController = PersonController(this)
+        txtName=findViewById<EditText>(R.id.nameText)
+        txtFLastName=findViewById<EditText>(R.id.fnameText)
+        txtSLastName=findViewById<EditText>(R.id.snameText)
+        txtPhone=findViewById<EditText>(R.id.phoneText)
+        txtEmail=findViewById<EditText>(R.id.emailText)
+        lbBirthdate=findViewById<TextView>(R.id.lbBirthdate_Person)
+
+        ResetDate ()
+
+        val btnSelectDate = findViewById<ImageButton>(R.id.btnSelectDate_person)
+        btnSelectDate.setOnClickListener(View.OnClickListener{ view ->
+            showDatePickerDialog()
+        })
+        
+        val btnSearch = findViewById<ImageButton>(R.id.btnSearchId_person)
+        btnSearch.setOnClickListener(View.OnClickListener{view ->
+            searchPerson(txtId.text.trim().toString())
+        })
+
+    }
+
+    private fun ResetDate (){
+        val calendar = Calendar.getInstance()
+        year = calendar.get(Calendar.YEAR)
+        day = calendar.get(Calendar.DAY_OF_MONTH)
+        month = calendar.get(Calendar.MONTH)
+
     }
 }
